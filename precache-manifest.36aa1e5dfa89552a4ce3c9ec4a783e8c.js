657f7f830745a7483bc28f95cc90712f3c114111
mominali12 <mominali1296@gmail.com>
2019-11-14 07:43:33 +0500

    Updates

---
+++ precache-manifest.36aa1e5dfa89552a4ce3c9ec4a783e8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1afefd793ed87b897846f3a3d1173fd",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b1914a5105b4854711e5",
+    "url": "/portfolio/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "bc410f8bebf2c7e45fb4",
+    "url": "/portfolio/static/js/2.e8af8b7a.chunk.js"
+  },
+  {
+    "revision": "b1914a5105b4854711e5",
+    "url": "/portfolio/static/js/main.4724a473.chunk.js"
+  },
+  {
+    "revision": "6b26cb3bc1b9147ba086",
+    "url": "/portfolio/static/js/runtime-main.86a05020.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portfolio/static/media/logo.25bf045c.svg"
+  }
+]);

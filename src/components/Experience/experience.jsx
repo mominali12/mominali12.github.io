f08132b3cbb6130cae5f56a9a9e610925990d8bd
mominali12 <mominali1296@gmail.com>
2019-11-15 15:01:02 +0500

    Update basic file structure

--- src/components/Experience/experience.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class experience extends Component {
-  render() {
-    return <div></div>;
-  }
-}
-
-export default experience;
--- src/components/projects/projects.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class projects extends Component {
-  render() {
-    return <div></div>;
-  }
-}
-
-export default projects;

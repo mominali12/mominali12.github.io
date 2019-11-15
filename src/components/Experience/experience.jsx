65ab4178e6847fbf96f27f45eba789c8bb86ff24
mominali12 <mominali1296@gmail.com>
2019-11-15 14:36:18 +0500

    Setting up basic file structure

---
+++ src/components/Experience/experience.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class experience extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default experience;
---
+++ src/components/projects/projects.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class projects extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default projects;
